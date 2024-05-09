308969b04b162e52830ad89de0bfb0ff35a462c3
Kris Lwin <65151308+HninLwin-byte@users.noreply.github.com>
2024-05-10 03:57:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import requests
 import openai
 from llama_index.llms.openai import OpenAI
 from llama_index.llms.gemini import Gemini
@@ -10,133 +9,67 @@ from llama_index.core import (
     Document, 
     SimpleDirectoryReader
 )
-
+from streamlit_extras.app_logo import add_logo
 import google.generativeai as genai
 
-# import dotenv,os
-# dotenv.load_dotenv()
+import dotenv,os
+dotenv.load_dotenv()
 
-
+st.set_page_config(page_title="UjjwalDeepXIXC", page_icon=r"kmutt_logo.png", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.header(":violet[SCG & KMUTT Chat]Bot",divider='rainbow', help = "This bot is designed by Ujjwal Deep to address all of your questions hehe")
+st.subheader("Hello! There, How can I help you Today- 👩‍💻")
+st.caption(":violet[what a] :orange[good day] :violet[to share what SCG is offering right now]:blue[ᓚᘏᗢ]")
 # st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-# # openai.api_key = st.secrets.openai_key
-# GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
-# genai.configure(api_key=GOOGLE_API_KEY)
+# st.title("KMUTT & SCG Chatbot")
+# add_logo("scg_logo.jpg")
+# add sidebar buttons
 
 
-# if "messages" not in st.session_state.keys(): # Initialize the chat messages history
-#     st.session_state.messages = [
-#         {"role": "assistant", "content": "Ask me a question about Concrete technology"}
-#     ]
-
-# @st.cache_resource(show_spinner=False)
-# def load_data():
-#     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-#         reader = SimpleDirectoryReader(input_dir="/Users/krislwin/Documents/LLM_Concrete/data", recursive=True)
-#         docs = reader.load_data()
-#         # llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert o$
-#         # index = VectorStoreIndex.from_documents(docs)
-#         # service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
-#         embed_model = GeminiEmbedding(
-#             model_name="models/embedding-001", title="this is a document"
-#             )
-#         service_context = ServiceContext.from_defaults(llm = Gemini(model="models/gemini-pro"), embed_model=embed_model,)
-#         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
-#         return index
-
-# index = load_data()
-
-# if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-#         st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-
-# if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-
-# for message in st.session_state.messages: # Display the prior chat messages
-#     with st.chat_message(message["role"]):
-#         st.write(message["content"])
-
-# # If last message is not from assistant, generate a new response
-# if st.session_state.messages[-1]["role"] != "assistant":
-#     with st.chat_message("assistant"):
-#         with st.spinner("Thinking..."):
-#             response = st.session_state.chat_engine.chat(prompt)
-#             st.write(response.response)
-#             message = {"role": "assistant", "content": response.response}
-#             st.session_state.messages.append(message) # Add response to message history
-
-
-# import streamlit as st
-# import openai
-# from llama_index.llms.openai import OpenAI
-# from llama_index.llms.gemini import Gemini
-# from llama_index.embeddings.gemini import GeminiEmbedding
-# from llama_index import (
-#     VectorStoreIndex, 
-#     ServiceContext, 
-#     Document, 
-#     SimpleDirectoryReader
-# )
-# import google.generativeai as genai
-
-# Set your Google API Key and OpenAI API Key directly in the code
-GOOGLE_API_KEY = "AIzaSyC8-OLPwz5M_GDhdQlV2QVdOveFQ8m-oag"
-# OPENAI_API_KEY = "your_openai_api_key_here"
-
-# Configure OpenAI and Google Generative AI
-# openai.api_key = OPENAI_API_KEY
+# add sidebar filters
+st.sidebar.slider("Slider", 0, 100, 50)
+st.sidebar.date_input("Date Input")
+# openai.api_key = st.secrets.openai_key
+GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
-# Streamlit page setup
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
-# Initialize chat messages history
-if "messages" not in st.session_state.keys():
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about concrete technology"}
+        {"role": "assistant", "content": "Ask me a question about Concrete technology"}
     ]
 
-# Function to load data and create the index
 @st.cache_resource(show_spinner=False)
-def download_pdf(url):
-    with st.spinner("Downloading PDF file..."):
-        response = requests.get(url)
-        with open("Sustainable_Concrete_Technology.pdf", "wb") as f:
-            f.write(response.content)
-    return "Sustainable_Concrete_Technology.pdf"
-
-# Function to load data and create the index
-@st.cache(show_spinner=False)
 def load_data():
-    # pdf_path = download_pdf("https://github.com/HninLwin-byte/Concrete_Chatbot_Test/blob/master/Sustainable_Concrete_Technology.pdf?raw=true")
-    with st.spinner(text="Loading and indexing the documents..."):
-        reader = SimpleDirectoryReader(input_dir="./devcontainer, recursive=True)
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        embed_model = GeminiEmbedding(model_name="models/embedding-001", title="This is a document")
-        llm = Gemini(model="models/gemini-pro")
-        service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model)
+        # llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert o$
+        # index = VectorStoreIndex.from_documents(docs)
+        # service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        embed_model = GeminiEmbedding(
+            model_name="models/embedding-001", title="this is a document"
+            )
+        service_context = ServiceContext.from_defaults(llm = Gemini(model="models/gemini-pro"), embed_model=embed_model,)
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
 index = load_data()
 
-# Initialize the chat engine
-if "chat_engine" not in st.session_state.keys():
-    st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-# Prompt for user input and save to chat history
-if prompt := st.chat_input("Your question"):
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-# Display the prior chat messages
-for message in st.session_state.messages:
+for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# If the last message is not from the assistant, generate a new response
+# If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = st.session_state.chat_engine.chat(prompt)
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message)  # Add response to message history
+            st.session_state.messages.append(message) # Add response to message history
