04a45c361a7ab7e4c9e02eef1cdb550959876b11
Kris Lwin <65151308+HninLwin-byte@users.noreply.github.com>
2024-05-10 22:02:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ st.set_page_config(page_title="UjjwalDeepXIXC", page_icon=r"scg_logo.jpg", layou
 st.header(":violet[SCG & KMUTT Chat]Bot",divider='rainbow', help = "This bot is designed by Ujjwal Deep to address all of your questions hehe")
 # st.subheader("Hello! There, How can I help you Today- :)")
 
-st.sidebar.image('scg_logo.jpg', width=60)
-st.sidebar.subheader("SCG")
+
 col1, col2 = st.columns([1, 4])
 
 # Display the logo in the first column
